7d4a1d34c126f172f5357c984f99864fdf66d298
Kunal <kunal.tanwar001@gmail.com>
2018-09-14 16:34:03 +0530

    Initial commit 1st till login

---
+++ app/src/main/java/com/example/prate/communication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.prate.communication;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.example.prate.communication.LoginActivity.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+ProgressBar progressBar;
+Handler handler;
+Intent intent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.starting_progressbar);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            progressBar.setVisibility(View.GONE);
+             intent =new Intent(MainActivity.this, LoginActivity.class);
+             startActivity(intent);
+             finish();
+            }
+        },2100);
+
+
+
+
+    }
+
+
+
+}
