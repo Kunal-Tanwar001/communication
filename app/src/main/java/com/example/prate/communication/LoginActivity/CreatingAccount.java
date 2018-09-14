89e78b819a43eefd0313bf237141053aad68948b
Kunal <kunal.tanwar001@gmail.com>
2018-09-14 17:32:21 +0530

    2nd chamge

---
+++ app/src/main/java/com/example/prate/communication/LoginActivity/CreatingAccount.java
@@ -0,0 +1,15 @@
+package com.example.prate.communication.LoginActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.prate.communication.R;
+
+public class CreatingAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_account);
+    }
+}
